49d2498d0ef30188afd7e3863617d191d5c3e038
almight3 <87767335+almight3@users.noreply.github.com>
2021-08-20 18:46:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button")
+const message = document.querySelector("#error-message");
+const numberOfNotes = document.querySelectorAll(".number-of-notes")
+const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];    
+
+checkButton.addEventListener("click" ,function validateAmount(){
+       message.style.display = "none"; 
+       if(billAmount.value>0){
+           if(cashGiven.value>=billAmount.value){
+             const amountToReturn = cashGiven.value - billAmount.value;
+             calculateAmountToReturn(amountToReturn);         
+            }
+           else{
+               showMessage("amount should be equal or greater than Bill amount");
+            }  
+        }
+       else{
+           showMessage("enter valid amount");
+       }
+
+       
+});
+
+ function  calculateAmountToReturn(amountToReturn){
+     for(let i=0; i < availableNotes.length; i++){
+         const noOfNotes = Math.trunc(amountToReturn/availableNotes[i])
+         amountToReturn %= availableNotes[i];
+         numberOfNotes[i].innerText = noOfNotes;
+    
+    }
+};
+
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+

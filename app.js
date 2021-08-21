af70aeb715ea0978f739744edff23ed5458a0d78
almight3 <shrivastavap136@gmail.com>
2021-08-21 23:25:59 +0530

    style
    add css

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 checkButton.addEventListener("click" ,function validateAmount(){
        message.style.display = "none"; 
        if(billAmount.value>0){
-           if(cashGiven.value >= billAmount.value){
+           if(cashGiven.value > billAmount.value){
              const amountToReturn = cashGiven.value - billAmount.value;
              calculateAmountToReturn(amountToReturn);         
             }
+           else if(cashGiven.value === billAmount.value){
+               showMessage("No Amount to be return")
+           }
+
            else{
                showMessage("amount should be equal or greater than Bill amount");
                console.log(cashGiven.value)
@@ -19,7 +23,7 @@ checkButton.addEventListener("click" ,function validateAmount(){
             }  
         }
        else{
-           showMessage("enter valid amount");
+           showMessage("Enter Valid Amount");
        }
 
        
